9c5203b02b68d9087472d7489b770efb6f91981c
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2023-05-31 02:48:02 +0600

    deploy: fix env file-7

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,12 @@
 # exit on error
 set -o errexit
 
-# poetry install
+
+ls -la settings/django/
+
+
+poetry install
 # python manage.py collectstatic --no-input
 # python manage.py makemigrations common errors api product
 # python manage.py migrate
 
-ls -la
-ls -la settings/
-ls -la ../
